d18a641577293a37b587d5a6bca8be95bac6fd66
Manh Hung <hungdlu@live.com>
2014-01-22 11:05:35 +0700

    Contact information to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,46 @@
 			<footer class="footer" role="contentinfo">
 			
+				<div class="row clearfix">
+					<hr>
+					<div class="small-12 medium-4 large-4 columns footer-entry">
+						<h2 class="footer-entry">Address</h2>
+						<p>
+							Graduate School of eLearning, </br>
+							Assumption University of Thailand, </br>
+							Srisakdi Charmonman IT Center, </br>
+							88 Moo 8 Bang Na-Trad Rd. Km.26, </br>
+							Bang Sao Thong, Samuthprakarn, </br>
+							Thailand 10540</br>
+						</p>
+					</div>
+
+					<div class="small-12 medium-4 large-4 columns footer-entry">
+						<h2 class="footer-entry">Contact Information</h2>
+						<p>
+							Telephone	+66 (0) 2723-2944 </br>
+							Telephone	+66 (0) 2723-2960 </br>
+							Fax	 		+66 (0) 2723-2959 </br>
+							Email		eLearning@au.edu </br>
+						</p>
+					</div>
+
+					<div class="small-12 medium-4 large-4 columns footer-entry">
+						<h2 class="footer-entry">Related Links</h2>
+						<p>
+							Assumption University </br>
+							M.S. (Management) </br>
+							M.S. (ICT) </br>
+							M.Ed. (Teaching and Technology) </br>
+							Ph.D. (eLearning Methodology) </br>
+							Ph.D. (Teaching and Technology) </br>
+						</p>
+					</div>
+
+					<hr>
+
+				</div>
+
+
 				<div id="inner-footer" class="row clearfix">
 				
 					<div class="large-12 medium-12 columns">
@@ -7,9 +48,10 @@
     						<?php joints_footer_links(); ?>
     					</nav>
     				</div>
+
 	               
 	                <div class="large-12 medium-12 columns">		
-						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>, Assumption University of Thailand. All rights reserved.</p>
 					</div>
 				
 				</div> <!-- end #inner-footer -->
