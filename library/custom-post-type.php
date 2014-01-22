2606b9c65db95ac7f065859b86ea6b3650ab0628
Manh Hung <hungdlu@live.com>
2014-01-22 16:44:24 +0700

    Update Custome Type --> Course

--- library/custom-post-type.php
+++ library/custom-post-type.php
@@ -16,24 +16,24 @@ in their own file.
 // let's create the function for the custom type
 function custom_post_example() { 
 	// creating (registering) the custom type 
-	register_post_type( 'custom_type', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
+	register_post_type( 'course', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
 	 	// let's now add all the options for this post type
 		array('labels' => array(
-			'name' => __('Custom Types', 'jointstheme'), /* This is the Title of the Group */
-			'singular_name' => __('Custom Post', 'jointstheme'), /* This is the individual type */
-			'all_items' => __('All Custom Posts', 'jointstheme'), /* the all items menu item */
-			'add_new' => __('Add New', 'jointstheme'), /* The add new menu item */
-			'add_new_item' => __('Add New Custom Type', 'jointstheme'), /* Add New Display Title */
+			'name' => __('Courses', 'jointstheme'), /* This is the Title of the Group */
+			'singular_name' => __('Course', 'jointstheme'), /* This is the individual type */
+			'all_items' => __('All Courses', 'jointstheme'), /* the all items menu item */
+			'add_new' => __('Add New Course', 'jointstheme'), /* The add new menu item */
+			'add_new_item' => __('Add New Course', 'jointstheme'), /* Add New Display Title */
 			'edit' => __( 'Edit', 'jointstheme' ), /* Edit Dialog */
-			'edit_item' => __('Edit Post Types', 'jointstheme'), /* Edit Display Title */
-			'new_item' => __('New Post Type', 'jointstheme'), /* New Display Title */
-			'view_item' => __('View Post Type', 'jointstheme'), /* View Display Title */
-			'search_items' => __('Search Post Type', 'jointstheme'), /* Search Custom Type Title */ 
+			'edit_item' => __('Edit Course', 'jointstheme'), /* Edit Display Title */
+			'new_item' => __('New Course', 'jointstheme'), /* New Display Title */
+			'view_item' => __('View Course', 'jointstheme'), /* View Display Title */
+			'search_items' => __('Search Course', 'jointstheme'), /* Search Course Title */ 
 			'not_found' =>  __('Nothing found in the Database.', 'jointstheme'), /* This displays if there are no entries yet */ 
 			'not_found_in_trash' => __('Nothing found in Trash', 'jointstheme'), /* This displays if there is nothing in the trash */
 			'parent_item_colon' => ''
 			), /* end of arrays */
-			'description' => __( 'This is the example custom post type', 'jointstheme' ), /* Custom Type Description */
+			'description' => __( 'Course for Bachelor', 'jointstheme' ), /* Course Description */
 			'public' => true,
 			'publicly_queryable' => true,
 			'exclude_from_search' => false,
@@ -41,8 +41,8 @@ function custom_post_example() {
 			'query_var' => true,
 			'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */ 
 			'menu_icon' => get_stylesheet_directory_uri() . '/library/images/custom-post-icon.png', /* the icon for the custom post type menu */
-			'rewrite'	=> array( 'slug' => 'custom_type', 'with_front' => false ), /* you can specify its url slug */
-			'has_archive' => 'custom_type', /* you can rename the slug here */
+			'rewrite'	=> array( 'slug' => 'course', 'with_front' => false ), /* you can specify its url slug */
+			'has_archive' => 'course', /* you can rename the slug here */
 			'capability_type' => 'post',
 			'hierarchical' => false,
 			/* the next one is important, it tells what's enabled in the post editor */
@@ -51,9 +51,9 @@ function custom_post_example() {
 	); /* end of register post type */
 	
 	/* this adds your post categories to your custom post type */
-	register_taxonomy_for_object_type('category', 'custom_type');
+	register_taxonomy_for_object_type('category', 'course');
 	/* this adds your post tags to your custom post type */
-	register_taxonomy_for_object_type('post_tag', 'custom_type');
+	register_taxonomy_for_object_type('post_tag', 'course');
 	
 } 
 
@@ -65,21 +65,21 @@ function custom_post_example() {
 	http://codex.wordpress.org/Function_Reference/register_taxonomy
 	*/
 	
-	// now let's add custom categories (these act like categories)
+	// now let's add course categories (these act like categories)
     register_taxonomy( 'custom_cat', 
-    	array('custom_type'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
+    	array('course'), /* if you change the name of register_post_type( 'course', then you have to change this */
     	array('hierarchical' => true,     /* if this is true, it acts like categories */             
     		'labels' => array(
-    			'name' => __( 'Custom Categories', 'jointstheme' ), /* name of the custom taxonomy */
-    			'singular_name' => __( 'Custom Category', 'jointstheme' ), /* single taxonomy name */
-    			'search_items' =>  __( 'Search Custom Categories', 'jointstheme' ), /* search title for taxomony */
-    			'all_items' => __( 'All Custom Categories', 'jointstheme' ), /* all title for taxonomies */
-    			'parent_item' => __( 'Parent Custom Category', 'jointstheme' ), /* parent title for taxonomy */
-    			'parent_item_colon' => __( 'Parent Custom Category:', 'jointstheme' ), /* parent taxonomy title */
-    			'edit_item' => __( 'Edit Custom Category', 'jointstheme' ), /* edit custom taxonomy title */
-    			'update_item' => __( 'Update Custom Category', 'jointstheme' ), /* update title for taxonomy */
-    			'add_new_item' => __( 'Add New Custom Category', 'jointstheme' ), /* add new title for taxonomy */
-    			'new_item_name' => __( 'New Custom Category Name', 'jointstheme' ) /* name title for taxonomy */
+    			'name' => __( 'Course Categories', 'jointstheme' ), /* name of the custom taxonomy */
+    			'singular_name' => __( 'Course Category', 'jointstheme' ), /* single taxonomy name */
+    			'search_items' =>  __( 'Search Course Categories', 'jointstheme' ), /* search title for taxomony */
+    			'all_items' => __( 'All Course Categories', 'jointstheme' ), /* all title for taxonomies */
+    			'parent_item' => __( 'Parent Course Category', 'jointstheme' ), /* parent title for taxonomy */
+    			'parent_item_colon' => __( 'Parent Course Category:', 'jointstheme' ), /* parent taxonomy title */
+    			'edit_item' => __( 'Edit Course Category', 'jointstheme' ), /* edit custom taxonomy title */
+    			'update_item' => __( 'Update Course Category', 'jointstheme' ), /* update title for taxonomy */
+    			'add_new_item' => __( 'Add New Course Category', 'jointstheme' ), /* add new title for taxonomy */
+    			'new_item_name' => __( 'New Course Category Name', 'jointstheme' ) /* name title for taxonomy */
     		),
     		'show_admin_column' => true, 
     		'show_ui' => true,
@@ -90,19 +90,19 @@ function custom_post_example() {
     
 	// now let's add custom tags (these act like categories)
     register_taxonomy( 'custom_tag', 
-    	array('custom_type'), /* if you change the name of register_post_type( 'custom_type', then you have to change this */
+    	array('course'), /* if you change the name of register_post_type( 'course', then you have to change this */
     	array('hierarchical' => false,    /* if this is false, it acts like tags */                
     		'labels' => array(
-    			'name' => __( 'Custom Tags', 'jointstheme' ), /* name of the custom taxonomy */
-    			'singular_name' => __( 'Custom Tag', 'jointstheme' ), /* single taxonomy name */
-    			'search_items' =>  __( 'Search Custom Tags', 'jointstheme' ), /* search title for taxomony */
-    			'all_items' => __( 'All Custom Tags', 'jointstheme' ), /* all title for taxonomies */
-    			'parent_item' => __( 'Parent Custom Tag', 'jointstheme' ), /* parent title for taxonomy */
-    			'parent_item_colon' => __( 'Parent Custom Tag:', 'jointstheme' ), /* parent taxonomy title */
-    			'edit_item' => __( 'Edit Custom Tag', 'jointstheme' ), /* edit custom taxonomy title */
-    			'update_item' => __( 'Update Custom Tag', 'jointstheme' ), /* update title for taxonomy */
-    			'add_new_item' => __( 'Add New Custom Tag', 'jointstheme' ), /* add new title for taxonomy */
-    			'new_item_name' => __( 'New Custom Tag Name', 'jointstheme' ) /* name title for taxonomy */
+    			'name' => __( 'Course Tags', 'jointstheme' ), /* name of the course taxonomy */
+    			'singular_name' => __( 'Course Tag', 'jointstheme' ), /* single taxonomy name */
+    			'search_items' =>  __( 'Search Course Tags', 'jointstheme' ), /* search title for taxomony */
+    			'all_items' => __( 'All Course Tags', 'jointstheme' ), /* all title for taxonomies */
+    			'parent_item' => __( 'Parent Course Tag', 'jointstheme' ), /* parent title for taxonomy */
+    			'parent_item_colon' => __( 'Parent Course Tag:', 'jointstheme' ), /* parent taxonomy title */
+    			'edit_item' => __( 'Edit Course Tag', 'jointstheme' ), /* edit course taxonomy title */
+    			'update_item' => __( 'Update Course Tag', 'jointstheme' ), /* update title for taxonomy */
+    			'add_new_item' => __( 'Add New Course Tag', 'jointstheme' ), /* add new title for taxonomy */
+    			'new_item_name' => __( 'New Course Tag Name', 'jointstheme' ) /* name title for taxonomy */
     		),
     		'show_admin_column' => true,
     		'show_ui' => true,
